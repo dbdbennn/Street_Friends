b58afa6e1248311722ea8b30aba3c79a0e3e490e
seungbeen <seungbin803@gmail.com>
2022-11-26 00:50:53 +0900

    api

--- js/openapi.js
+++ js/openapi.js
@@ -1,17 +1,9 @@
-// 유기동물 조회
-// bgnde : 유기날짜(시작)
-// upkind : 축종코드
-// upr_cd : 시도코드
-// state : 보호상태
-// kindCd : 품종
-// colorCd : 색상
-// age : 나이
-// weight : 체중
-const KEY = "rb7O7oSfVkWUcexLi2pn6VaPa82kTvaE3BtiP%2BBz21WfZGE4GVF%2BoCizRopEZjsi4vxBVpkGQa%2Fsx%2BT6PH3QyQ%3D%3D";
-const upr_cd = "6110000";
-const upkind = "417000";
-let url = `http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?`
-        + `KEY=${KEY}`
-        + `upr_cd=${upr_cd}`
-        + `upkind=${upkind}`;
+// 유기동물조회
+// https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15098931
+
+// 신청주소 : https://openapi.gg.go.kr/OrganicAnimalProtectionFacilit
+const KEY = "afdc0e74c5b94d95914fb2651627fc3a";
+
+var url = `https://openapi.gg.go.kr/OrganicAnimalProtectionFacilit?` /*URL*/
+        + `KEY=${KEY}`;
 console.log(url);
